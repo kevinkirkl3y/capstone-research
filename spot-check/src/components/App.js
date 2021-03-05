46df79952586a88f7dcb4001553b5f74456d792e
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-03-05 10:36:46 -0800

    Troubleshoot Google Maps Api Key Issue

--- spot-check/src/components/App.js
+++ spot-check/src/components/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import Header from './Header';
+import SpotControl from './SpotController';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path='/signin'>
-          <SignIn />
-        </Route>
-        <Route path='/'>
-          <SpotControl />
-        </Route>
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Header />
+    //   <Switch>
+    //     {/* <Route path='/signin'>
+    //       <SignIn />
+    //     </Route> */}
+    //     <Route path='/'>
+    //       <SpotControl />
+    //     </Route>
+    //   </Switch>
+    // </Router>
+    <>
+     <Header/>
+     <SpotControl/>
+    </>
   );
 }
 
---
+++ spot-check/src/components/SpotController.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SpotMap from './SpotMap';
+
+class SpotControl extends React.Component {
+
+  render() {
+    return (
+      <>
+        <SpotMap/>
+      </>
+    )
+  }
+}
+export default SpotControl;
